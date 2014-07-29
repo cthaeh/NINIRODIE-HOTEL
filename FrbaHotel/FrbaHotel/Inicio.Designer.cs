8ed2c578d95b44e592c95ffbff2a24dce9527b57
cthaeh <diegojbarchiesi@hotmail.com.ar>
2014-07-29 04:33:07 -0300

    Ventana Inicio y Login

---
+++ FrbaHotel/FrbaHotel/Inicio.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FrbaHotel
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Login = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.reserva = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.Login.Location = new System.Drawing.Point(607, 37);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(75, 23);
+            this.Login.TabIndex = 8;
+            this.Login.Text = "Logearse";
+            this.Login.UseVisualStyleBackColor = true;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(295, 178);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(75, 23);
+            this.Salir.TabIndex = 7;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // reserva
+            // 
+            this.reserva.Location = new System.Drawing.Point(295, 72);
+            this.reserva.Name = "reserva";
+            this.reserva.Size = new System.Drawing.Size(75, 45);
+            this.reserva.TabIndex = 6;
+            this.reserva.Text = "Realizar Reserva";
+            this.reserva.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(694, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(694, 266);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.reserva);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Inicio";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.Button reserva;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+    }
+}
+
